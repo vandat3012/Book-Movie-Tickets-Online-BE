4658cca18727910f5f15d897c5a04e537e082c8b
vandat3012 <luongvandat301220@gmail.com>
2024-06-03 13:34:40 +0700

    add information for account

---
+++ src/main/java/com/example/bookmovieticketsonline/repository/IMovieRepository.java
@@ -0,0 +1,4 @@
+package com.example.bookmovieticketsonline.repository;
+
+public interface IMovieRepository {
+}
---
+++ src/main/java/com/example/bookmovieticketsonline/service/IMovieService.java
@@ -0,0 +1,4 @@
+package com.example.bookmovieticketsonline.service;
+
+public interface IMovieService {
+}
